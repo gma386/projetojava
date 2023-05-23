8152a00fb43f3316f57b1848f0461dc6eebbbe1f
Guilherme <guilhermebuhabreu@gmail.com>
2023-05-23 10:29:38 -0400

    Segundo commit

--- primeiro-programa-java/src/cursojava/executavel/PriemiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PriemiraClasseJava.java
@@ -4,5 +4,6 @@ public class PriemiraClasseJava {
 	
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em java");
+		System.out.println("Testando segundo commit");
 	}
 }
